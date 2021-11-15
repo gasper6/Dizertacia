84b1b521455f4d2b27f5561ddd32b29a6c668cca
gasper6 <gasper6@uniba.sk>
2021-11-15 19:05:57 +0100

    Update Linear sparse ode testing.py

--- Linear sparse ode testing.py
+++ Linear sparse ode testing.py
@@ -6,26 +6,44 @@ Created on Thu Oct 28 21:16:39 2021
 """
 
 import numpy as np
-from scipy.sparse import csc_matrix, random
-from scipy.sparse.linalg import expm
+from scipy.sparse import random, diags
+from scipy.sparse.linalg import expm_multiply
 from scipy.integrate import solve_ivp
 
+t_max = 100
 
 N = 5000
 
 A = random(N, N, density=2/N, format="csc")
-eA = expm(A)
+A = A - diags(A.sum(1).A1)
 
+x0 = np.random.rand(N)
+x0 /= x0.sum() * 10**10
+
+eA = expm_multiply(A, x0, 0, t_max, num=2, endpoint=True)[-1]
+
+
+"""
+print("expm function")
+%timeit eA = expm_multiply(A, x0, 0, t_max, num=2, endpoint=True)
+"""
 
 def dt(t, y):
     return A @ y
 
-t_max = 100
 
-y0 = np.random.randn(N)
 
 sols = []
-
+"""
 for m in ("RK23", "RK45", "DOP853", "Radau", "BDF", "LSODA"):
-    sols.append(solve_ivp(dt, (0, t_max), y0, jac=A, method=m, t_eval=(t_max,)))
+    sols.append(solve_ivp(dt, (0, t_max), x0, jac=A, method=m, t_eval=(t_max,)))
+"""
+  
+for m in ("RK23", "RK45", "DOP853"):
+    sols.append(solve_ivp(dt, (0, t_max), x0, jac=A, method=m, t_eval=(t_max,)))
     
+"""
+for m in ("RK23", "RK45", "DOP853"):
+    print(m)
+    %timeit solve_ivp(dt, (0, t_max), x0, jac=A, method=m, t_eval=(t_max,), vectorized=True)
+"""
